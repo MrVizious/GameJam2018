1fb6bf7712529a7cdf834ac7790e34f82aad109c
MrVizious <mrvizious@gmail.com>
2018-05-02 13:41:35 +0200

    Pull pre compliación

---
+++ Assets/Scripts/playerJumpScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class playerJumpScript : MonoBehaviour {
+    
+    //Components
+    Rigidbody2D rb;
+    Animator animator;
+
+    //Public variables
+    public float jumpForce = 5f;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
